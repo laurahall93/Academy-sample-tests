412584be00b695615d2618512a6999156f30dacb
Laura Hall <podinjshmi1993@gmail.com>
2023-06-18 13:02:35 +0100

    greeting function added and tested

--- greeting.js
+++ greeting.js
@@ -1,1 +1,23 @@
 //psoudo code
+/*Program : individualGreeting
+input : array of strings
+output : array of strings
+
+function individualGreetings(names)
+set greeting = [];
+for each names in names 
+greeting.push(`"Hello ${name}!"`);
+
+return greeting
+*/
+
+
+function individualGreetings(names) {
+    let greeting = [];
+    for(let name of names){
+        greeting.push(`Hello,  ${name}!`);
+    }
+    return greeting;
+}
+
+console.log(individualGreetings(["Ash", "Beth", "Ciara"]));
